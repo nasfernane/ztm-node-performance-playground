7b2a53732ada2d5b1353e430bd61c9505da303ce
Nassim Fernane <nasfernane@gmail.com>
2023-07-13 08:10:38 +0200

    ajout pm2

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
+// const cluster = require('cluster');
+// const os = require('os');
 // const availableParallelism = require('node:os');
 
 const app = express();
 
 const PORT = 3000;
-const CPUS_NUMBER = os.availableParallelism();
+// const CPUS_NUMBER = os.availableParallelism();
 
 // subtilité pour que ça marche sur windows
-cluster.schedulingPolicy = cluster.SCHED_RR;
+// cluster.schedulingPolicy = cluster.SCHED_RR;
 
 function delay(duration) {
   const startTime = Date.now();
@@ -33,16 +33,24 @@ app.get('/timer', (req, res) => {
 })
 
 
-if (cluster.isMaster) {
-  console.log('Master has been started')
+// if (cluster.isMaster) {
+//   console.log('Master has been started')
 
-  for (let i = 1; i <= CPUS_NUMBER; i++) {
-    cluster.fork().on('disconnect', () => {
-    console.log(`Worker ${i} disconnected`);
-  });
-  }
-} else {
-  console.log('worker listening');
-  app.listen(PORT);
-}
+//   for (let i = 1; i <= CPUS_NUMBER; i++) {
+//     cluster.fork().on('disconnect', () => {
+//       console.log(`Worker ${i} disconnected`);
+//     });
+//   }
+// } else {
+//   console.log('worker listening');
+//   app.listen(PORT);
+// }
+
+app.listen(PORT, () => {
+  console.log(`server listening at port ${PORT}`)
+});
+
+// Au lieu de créer un cluster qu'on fork selon le nombre de cpus disponibles, on peut lancer via pm2 qui intègre directement le clustering en spécifiant le nombre d'instances ou en ouvrant le nombre max
+// ex : pm2 start server.js -i 8
+// ex : pm2 start server.js -i max
 
